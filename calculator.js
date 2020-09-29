d62895280a5a0df41477c85040b1182ea213f7ca
imhilla <hillaryodhiambo282@gmail.com>
2020-09-29 07:39:11 +0300

    add calculator object and test

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let calculator = {
+  add: (x, y) => {
+    var x = x + y
+    return x
+  },
+  subtract: (x, y) => {
+    var x = x - y
+    return x
+  },
+  divide: (x, y) => {
+    var x = x / y
+    return x
+  },
+  multiply: (x, y) => {
+    var x = x * y
+    return x
+  }
+}
+export default calculator
