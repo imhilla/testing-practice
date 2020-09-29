49dc02d643f4e7ac7e62827f65a569c9acd4dbfb
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-09-29 19:35:15 +0500

    Adds Tests for Objects

--- arrayAnalysis.test.js
+++ arrayAnalysis.test.js
@@ -4,6 +4,11 @@ test('Analyze the Max Value in array', () => {
   expect(analyze([1, 5, 6]).max).toBe(6);
 });
 
+test('object assignment', () => {
+  const data = analyze([1, 5, 6]);
+  expect(data).toEqual({ average: 4, min: 1, max: 6, length: 3 });
+});
+
 test('Analyze the Min Value in array', () => {
   expect(analyze([1, 5, 6]).min).toBe(1);
 });
